c62653ff3b7a2d633bcf76c47d6df7ca5cd20a75
Terence <tmangram@gmail.com>
2017-10-24 21:50:14 +0900

    Added supporter page route

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import FeaturedPage from '../containers/FeaturedPage'
 import EventsPage from '../containers/EventsPage'
+import SupporterPage from '../containers/SupporterPage'
 import Banner from '../components/Banner'
 
 class Main extends Component{
@@ -11,6 +12,7 @@ class Main extends Component{
 				<Banner/>
 				<Route exact path="/" component={ FeaturedPage }/>
 				<Route path="/event/:id" component={ EventsPage }/>
+				<Route path="/supporter/:id" component={ SupporterPage }/>
 			</div>
 		)
 	}
