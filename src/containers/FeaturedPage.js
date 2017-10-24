fa3317e818ca1ce63c55499c035cb22f2f1b57f5
Terence <tmangram@gmail.com>
2017-10-24 21:50:42 +0900

    Added supporter carousel data

--- src/containers/FeaturedPage.js
+++ src/containers/FeaturedPage.js
@@ -6,7 +6,7 @@ class FeaturedPage extends Component{
 	constructor() {
 		super()
 		this.state = {
-			data: [
+			event_data: [
 				{
 					title: "庄内緑地公園バーベキュー",
 					category: "Social",
@@ -14,7 +14,7 @@ class FeaturedPage extends Component{
 					date: "10/31/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/200x160",
+					img: "https://via.placeholder.com/220x160",
 					url: "/event/1"
 				},
 				{
@@ -24,7 +24,7 @@ class FeaturedPage extends Component{
 					date: "11/01/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/200x160",
+					img: "https://via.placeholder.com/220x160",
 					url: "/event/2"
 				},
 				{
@@ -34,7 +34,7 @@ class FeaturedPage extends Component{
 					date: "11/02/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/200x160",
+					img: "https://via.placeholder.com/220x160",
 					url: "/event/3"
 				},
 				{
@@ -44,7 +44,7 @@ class FeaturedPage extends Component{
 					date: "11/03/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/200x160",
+					img: "https://via.placeholder.com/220x160",
 					url: "/event/4"
 				},
 				{
@@ -54,7 +54,7 @@ class FeaturedPage extends Component{
 					date: "11/04/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/200x160",
+					img: "https://via.placeholder.com/220x160",
 					url: "/event/5"
 				},
 				{
@@ -64,7 +64,7 @@ class FeaturedPage extends Component{
 					date: "11/05/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/200x160",
+					img: "https://via.placeholder.com/220x160",
 					url: "/event/6"
 				},
 				{
@@ -74,7 +74,7 @@ class FeaturedPage extends Component{
 					date: "11/06/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/200x160",
+					img: "https://via.placeholder.com/220x160",
 					url: "/event/7"
 				},
 				{
@@ -84,7 +84,7 @@ class FeaturedPage extends Component{
 					date: "11/07/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/200x160",
+					img: "https://via.placeholder.com/220x160",
 					url: "/event/8"
 				},
 				{
@@ -94,7 +94,7 @@ class FeaturedPage extends Component{
 					date: "11/08/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/200x160",
+					img: "https://via.placeholder.com/220x160",
 					url: "/event/9"
 				},
 				{
@@ -104,8 +104,70 @@ class FeaturedPage extends Component{
 					date: "11/09/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/200x160",
+					img: "https://via.placeholder.com/220x160",
 					url: "/event/10"
+				},
+			],
+			supporter_data: [
+				{
+					supporter: "Maeda Atsuko",
+					experience: "AKB48, Actress",
+					languages: "日本語、中国語、英語",
+					time: "10:00 - 17:30",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://via.placeholder.com/220x160",
+					url: "/supporter/1"
+				},
+				{
+					supporter: "Matsumoto Jun",
+					experience: "Arashi member, Actor",
+					languages: "日本語、中国語、英語",
+					time: "12:00 - 18:30",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://via.placeholder.com/220x160",
+					url: "/supporter/2"
+				},
+				{
+					supporter: "Hirai Momo",
+					experience: "Twice member",
+					languages: "日本語、英語",
+					time: "15:00 - 21:00",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://via.placeholder.com/220x160",
+					url: "/supporter/3"
+				},
+				{
+					supporter: "Sugi chan",
+					experience: "Comedian",
+					languages: "日本語、中国語",
+					time: "8:30 - 15:00",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://via.placeholder.com/220x160",
+					url: "/supporter/4"
+				},
+				{
+					supporter: "Imoto Ayako",
+					experience: "TV show presenter",
+					languages: "日本語、英語",
+					time: "5:00 - 12:30",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://via.placeholder.com/220x160",
+					url: "/supporter/5"
+				},
+				{
+					supporter: "Edogawa Conan",
+					experience: "Detective",
+					languages: "日本語、中国語、英語",
+					time: "2:00 - 20:00",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://via.placeholder.com/220x160",
+					url: "/supporter/6"
 				},
 			]
 		}
@@ -113,7 +175,7 @@ class FeaturedPage extends Component{
 
 	render() {
 		return (
-			<Featured data={ this.state.data }/>
+			<Featured eventData={ this.state.event_data } supporterData={ this.state.supporter_data } />
 		)
 	}
 }
