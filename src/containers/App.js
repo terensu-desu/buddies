e74c88cca80820208dc433a15c34cdb57407208d
Terence <tmangram@gmail.com>
2017-10-24 19:09:23 +0900

    Added hashrouter instead of browserrouter

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom' //can use HashRouter if running into trouble on builds
+import { HashRouter as Router } from 'react-router-dom' //can use HashRouter if running into trouble on builds
 import Navbar from '../components/Navbar'
 import Main from '../components/Main'
 
