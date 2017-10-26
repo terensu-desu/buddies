09d6f26dc3e8b1b44091d5a749fa34572788f10c
Terence <tmangram@gmail.com>
2017-10-26 22:48:56 +0900

    Cleaned up structure, added social link icons.

--- src/components/Supporter.js
+++ src/components/Supporter.js
@@ -8,49 +8,75 @@ class Supporter extends Component{
 			<div className="row">
 				<div className="col s12">
 					<div className="col s8">
-						<h4><span>{ data.supporter }</span>さん</h4>
-						<div className="close-text">{ data.supporter_intro }</div>
+						<h4 className="no-margin-top"><span>{ data.supporter }</span>さん</h4>
+						<div className="row no-margin-bot">
+							<div className="col s12">
+								<div className="close-text">{ data.supporter_intro }</div>
+							</div>
+						</div>
 						<hr/>
-						<br/>
-						<div className="close-text"><i className="material-icons left">person</i>{ data.status }</div>
-						<div className="close-text"><i className="material-icons left">language</i>ガイドの言語：{ data.languages }</div>
-						<div className="close-text"><i className="material-icons left">build</i>{ data.experience }</div>
-						<div className="close-text"><i className="material-icons left">school</i>{ data.skills }</div>
+						<div className="row no-margin-bot">
+							<div className="col s12">
+								<div className="close-text"><i className="material-icons left">person</i>{ data.status ? "Online!" : "Offline" }</div>
+								<div className="close-text"><i className="material-icons left">language</i>ガイドの言語：{ data.languages }</div>
+								<div className="close-text"><i className="material-icons left">build</i>{ data.experience }</div>
+								<div className="close-text"><i className="material-icons left">school</i>{ data.skills }</div>
+							</div>
+						</div>
 						<hr/>
 						<div className="row">
-							<div className="col s2">
+							<div className="col s12">
 								<div className="close-text">
 									サポーターのオープンタイム
 								</div>
-							</div>
-							<div className="col s10">
 								<div className="close-text">
 									{ data.open_time }
 								</div>
-							</div>
-						</div>
-						<div className="row">
-							<div className="col s2">
+								<hr/>
 								<div className="close-text">
 									エリア:
 								</div>
-							</div>
-							<div className="col s10">
 								<div className="close-text">
-									{ data.area_info }
+									The next event will be held on 10/31/2017 and be available for all.
+									<iframe className="area-map" title="map" src="https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I" allowFullScreen></iframe>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div className="col s4">
-						<img className="main-page-image" src={ data.img } alt="event promo"/>
-						<div className="close-text">
-							<a className="btn appointment-btn">Appointment!</a>
+						<img className="responsive-img" src={ data.img } alt="event promo"/>
+						<div className="row no-margin-bot">
+							<div className="col s6">
+								<div className="close-text">
+									<div className="star-ratings-css left">
+									  <div className="star-ratings-css-top" style={{width: "90%"}}><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+									  <div className="star-ratings-css-bottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+									</div>
+									<p className="reviews-text">12件のレビュー</p>
+								</div>
+							</div>
+							<div className="col s6">
+								<div className="center close-text">
+									<a className="btn appointment-btn orange darken-3">Appointment!</a>
+								</div>
+							</div>
 						</div>
-						<div className="close-text"><i className="material-icons left">star_rate</i>12件のレビュー</div>
-						<div className="center close-text">
-							<img className="thumbnail" src={ data.thumb1 } alt="event promo"/>
-							<img className="thumbnail" src={ data.thumb2 } alt="event promo"/>
+						<div className="row no-margin-bot">
+							<div className="col s12">
+								<div className="center thumbnail-container close-text">
+									<img className="thumbnail" src={ data.thumb1 } alt="event promo"/>
+									<img className="thumbnail" src={ data.thumb2 } alt="event promo"/>
+								</div>
+							</div>
+						</div>
+						<div className="row no-margin-bot">
+							<div className="col s12 center">
+								<a href="#!"><i className="vit-accent social-icons fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>
+								<a href="#!"><i className="vit-accent social-icons fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>
+								<a href="#!"><i className="vit-accent social-icons fa fa-google-plus-square fa-2x" aria-hidden="true"></i></a>
+								<a href="#!"><i className="vit-accent social-icons fa fa-envelope fa-2x" aria-hidden="true"></i></a>
+								<a href="#!"><i className="vit-accent social-icons fa fa-heart fa-2x" aria-hidden="true"></i></a>
+							</div>
 						</div>
 					</div>
 				</div>
