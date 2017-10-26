26a613617029866c372afc60fcc48b05d59c11a6
Terence <tmangram@gmail.com>
2017-10-26 22:48:02 +0900

    Added real photo placeholders from PlaceIMG, added events data for carousel

--- src/containers/FeaturedPage.js
+++ src/containers/FeaturedPage.js
@@ -6,7 +6,7 @@ class FeaturedPage extends Component{
 	constructor() {
 		super()
 		this.state = {
-			event_data: [
+			life_data: [
 				{
 					title: "庄内緑地公園バーベキュー",
 					category: "Social",
@@ -14,8 +14,8 @@ class FeaturedPage extends Component{
 					date: "10/31/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/220x160",
-					url: "/event/1"
+					img: "https://placeimg.com/300/250/people?t=1509023119587",
+					url: "/life/1"
 				},
 				{
 					title: "庄内緑地公園バーベキュー",
@@ -24,8 +24,8 @@ class FeaturedPage extends Component{
 					date: "11/01/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/220x160",
-					url: "/event/2"
+					img: "https://placeimg.com/300/250/any",
+					url: "/life/2"
 				},
 				{
 					title: "庄内緑地公園バーベキュー",
@@ -34,8 +34,8 @@ class FeaturedPage extends Component{
 					date: "11/02/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/220x160",
-					url: "/event/3"
+					img: "https://placeimg.com/300/250/any?t=1509023596355",
+					url: "/life/3"
 				},
 				{
 					title: "庄内緑地公園バーベキュー",
@@ -44,8 +44,8 @@ class FeaturedPage extends Component{
 					date: "11/03/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/220x160",
-					url: "/event/4"
+					img: "https://placeimg.com/300/250/people?t=1509023130422",
+					url: "/life/4"
 				},
 				{
 					title: "庄内緑地公園バーベキュー",
@@ -54,8 +54,8 @@ class FeaturedPage extends Component{
 					date: "11/04/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/220x160",
-					url: "/event/5"
+					img: "https://placeimg.com/300/250/any?t=1509023688995",
+					url: "/life/5"
 				},
 				{
 					title: "庄内緑地公園バーベキュー",
@@ -64,8 +64,8 @@ class FeaturedPage extends Component{
 					date: "11/05/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/220x160",
-					url: "/event/6"
+					img: "https://placeimg.com/300/250/any?t=1509023796532",
+					url: "/life/6"
 				},
 				{
 					title: "庄内緑地公園バーベキュー",
@@ -74,8 +74,8 @@ class FeaturedPage extends Component{
 					date: "11/06/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/220x160",
-					url: "/event/7"
+					img: "https://placeimg.com/300/250/any?t=1509023807155",
+					url: "/life/7"
 				},
 				{
 					title: "庄内緑地公園バーベキュー",
@@ -84,8 +84,8 @@ class FeaturedPage extends Component{
 					date: "11/07/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/220x160",
-					url: "/event/8"
+					img: "https://placeimg.com/300/250/tech",
+					url: "/life/8"
 				},
 				{
 					title: "庄内緑地公園バーベキュー",
@@ -94,8 +94,122 @@ class FeaturedPage extends Component{
 					date: "11/08/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/220x160",
-					url: "/event/9"
+					img: "https://placeimg.com/300/250/tech?t=1509023264369",
+					url: "/life/9"
+				},
+			],
+			supporter_data: [
+				{
+					supporter: "Maeda Yuna",
+					experience: "Psychologist",
+					languages: "日本語、英語",
+					time: "10:00 - 17:30",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://placeimg.com/300/250/any/sepia",
+					url: "/supporter/1"
+				},
+				{
+					supporter: "Matsumoto Jon",
+					experience: "Financial Specialist",
+					languages: "日本語、中国語、英語",
+					time: "12:00 - 18:30",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://placeimg.com/300/250/people?t=1509023124699",
+					url: "/supporter/2"
+				},
+				{
+					supporter: "Hirai Moka",
+					experience: "Privacy Specialist",
+					languages: "日本語、英語",
+					time: "15:00 - 21:00",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://placeimg.com/300/250/people?t=1509023127964",
+					url: "/supporter/3"
+				},
+				{
+					supporter: "Sugiura Chan",
+					experience: "Suicide Prevention",
+					languages: "日本語、中国語",
+					time: "8:30 - 15:00",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://placeimg.com/300/250/people?t=1509023146097",
+					url: "/supporter/4"
+				},
+				{
+					supporter: "Imoto Ayano",
+					experience: "Career Consultant",
+					languages: "日本語、英語",
+					time: "5:00 - 12:30",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://placeimg.com/300/250/people?t=1509023171413",
+					url: "/supporter/5"
+				},
+				{
+					supporter: "Edogawa Kodai",
+					experience: "Detective",
+					languages: "日本語、中国語、英語",
+					time: "2:00 - 20:00",
+					rating: "5/5 Stars",
+					price: "¥3000 / 時間",
+					img: "https://placeimg.com/300/250/people?t=1509023155944",
+					url: "/supporter/6"
+				},
+			],
+			event_data: [
+				{
+					title: "庄内緑地公園バーベキュー",
+					category: "Social",
+					host: "山田涼介",
+					date: "11/04/2017",
+					rating: "5/5 Stars",
+					price: "¥3000 / 人",
+					img: "https://placeimg.com/300/250/any?t=1509023859468",
+					url: "/events/1"
+				},
+				{
+					title: "庄内緑地公園バーベキュー",
+					category: "Social",
+					host: "山田涼介",
+					date: "11/05/2017",
+					rating: "5/5 Stars",
+					price: "¥3000 / 人",
+					img: "https://placeimg.com/300/250/any?t=1509024027842",
+					url: "/events/2"
+				},
+				{
+					title: "庄内緑地公園バーベキュー",
+					category: "Social",
+					host: "山田涼介",
+					date: "11/06/2017",
+					rating: "5/5 Stars",
+					price: "¥3000 / 人",
+					img: "https://placeimg.com/300/250/any?t=1509024039437",
+					url: "/events/3"
+				},
+				{
+					title: "庄内緑地公園バーベキュー",
+					category: "Social",
+					host: "山田涼介",
+					date: "11/07/2017",
+					rating: "5/5 Stars",
+					price: "¥3000 / 人",
+					img: "https://placeimg.com/300/250/any?t=1509024050635",
+					url: "/events/4"
+				},
+				{
+					title: "庄内緑地公園バーベキュー",
+					category: "Social",
+					host: "山田涼介",
+					date: "11/08/2017",
+					rating: "5/5 Stars",
+					price: "¥3000 / 人",
+					img: "https://placeimg.com/300/250/any?t=1509024070823",
+					url: "/events/5"
 				},
 				{
 					title: "庄内緑地公園バーベキュー",
@@ -104,70 +218,18 @@ class FeaturedPage extends Component{
 					date: "11/09/2017",
 					rating: "5/5 Stars",
 					price: "¥3000 / 人",
-					img: "https://via.placeholder.com/220x160",
-					url: "/event/10"
-				},
-			],
-			supporter_data: [
-				{
-					supporter: "Maeda Atsuko",
-					experience: "AKB48, Actress",
-					languages: "日本語、中国語、英語",
-					time: "10:00 - 17:30",
-					rating: "5/5 Stars",
-					price: "¥3000 / 時間",
-					img: "https://via.placeholder.com/220x160",
-					url: "/supporter/1"
+					img: "https://placeimg.com/300/250/any?t=1509024091387",
+					url: "/events/6"
 				},
 				{
-					supporter: "Matsumoto Jun",
-					experience: "Arashi member, Actor",
-					languages: "日本語、中国語、英語",
-					time: "12:00 - 18:30",
+					title: "庄内緑地公園バーベキュー",
+					category: "Social",
+					host: "山田涼介",
+					date: "11/10/2017",
 					rating: "5/5 Stars",
-					price: "¥3000 / 時間",
-					img: "https://via.placeholder.com/220x160",
-					url: "/supporter/2"
-				},
-				{
-					supporter: "Hirai Momo",
-					experience: "Twice member",
-					languages: "日本語、英語",
-					time: "15:00 - 21:00",
-					rating: "5/5 Stars",
-					price: "¥3000 / 時間",
-					img: "https://via.placeholder.com/220x160",
-					url: "/supporter/3"
-				},
-				{
-					supporter: "Sugi chan",
-					experience: "Comedian",
-					languages: "日本語、中国語",
-					time: "8:30 - 15:00",
-					rating: "5/5 Stars",
-					price: "¥3000 / 時間",
-					img: "https://via.placeholder.com/220x160",
-					url: "/supporter/4"
-				},
-				{
-					supporter: "Imoto Ayako",
-					experience: "TV show presenter",
-					languages: "日本語、英語",
-					time: "5:00 - 12:30",
-					rating: "5/5 Stars",
-					price: "¥3000 / 時間",
-					img: "https://via.placeholder.com/220x160",
-					url: "/supporter/5"
-				},
-				{
-					supporter: "Edogawa Conan",
-					experience: "Detective",
-					languages: "日本語、中国語、英語",
-					time: "2:00 - 20:00",
-					rating: "5/5 Stars",
-					price: "¥3000 / 時間",
-					img: "https://via.placeholder.com/220x160",
-					url: "/supporter/6"
+					price: "¥3000 / 人",
+					img: "https://placeimg.com/300/250/any?t=1509024097862",
+					url: "/events/7"
 				},
 			]
 		}
@@ -175,7 +237,7 @@ class FeaturedPage extends Component{
 
 	render() {
 		return (
-			<Featured eventData={ this.state.event_data } supporterData={ this.state.supporter_data } />
+			<Featured lifeData={ this.state.life_data } supporterData={ this.state.supporter_data } eventData={ this.state.event_data } />
 		)
 	}
 }
