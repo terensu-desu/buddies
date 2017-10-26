1bdafd07ff6903ddd5b14bf9edb220bd67558fc6
Terence <tmangram@gmail.com>
2017-10-26 22:52:06 +0900

    Changed subheader text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,11 +7,11 @@ class Banner extends Component{
 				<div className="section">
 					<div className="container center white-text">
 						<h2>Vitamin</h2>
-						<h4>心と生活のサプリメント</h4>
+						<h4 className="banner-subtitle">People <i className="material-icons">share</i> Time <i className="material-icons">share</i> Experiences</h4>
 					</div>
 				</div>
 				<div className="parallax">
-					<img className="banner" src="https://i.imgur.com/Q3NeZj1.jpg?1" alt="banner"/>
+					<img className="banner" src="https://i.imgur.com/3VuMkuL.jpg?2" alt="banner"/>
 				</div>
 			</div>
 		)
