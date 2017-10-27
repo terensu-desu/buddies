0ae87bc010c67aa3d644fb654ba8e1c9a29021f0
Terence <tmangram@gmail.com>
2017-10-27 21:14:30 +0900

    Add route for EventsPage plus requirement

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 import FeaturedPage from '../containers/FeaturedPage'
 import LifePage from '../containers/LifePage'
 import SupporterPage from '../containers/SupporterPage'
+import EventsPage from '../containers/EventsPage'
 import Banner from '../components/Banner'
 
 class Main extends Component{
@@ -11,8 +12,9 @@ class Main extends Component{
 			<div>
 				<Banner/>
 				<Route exact path="/" component={ FeaturedPage }/>
-				<Route path="/life/:id" component={ LifePage }/>
+				<Route path="/life/:id" component={ LifePage } />
 				<Route path="/supporter/:id" component={ SupporterPage }/>
+				<Route path="/events/:id" component={ EventsPage }/>
 			</div>
 		)
 	}
