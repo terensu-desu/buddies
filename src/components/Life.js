83289cf9d54fc9b61dbca559baa3e4ab6f215a20
Terence <tmangram@gmail.com>
2017-10-29 19:01:06 +0900

    Change to price and appointment button for styling and icon info

--- src/components/Life.js
+++ src/components/Life.js
@@ -22,34 +22,47 @@ class Life extends Component{
 						<br/>
 						<div className="row no-margin-bot">
 							<div className="col s12">
-								<div className="close-text"><i className="material-icons left">date_range</i>{ data.date }</div>
-								<div className="close-text"><i className="material-icons left">access_time</i>{ data.time }</div>
-								<div className="close-text"><i className="material-icons left">note</i>{ data.memo }</div>
+								<div className="close-text"><i className="material-icons left">date_range</i>次の日: { data.date }</div>
+								<div className="close-text"><i className="material-icons left">access_time</i>予定時刻: { data.time }</div>
+								<div className="close-text"><i className="material-icons left">note</i>持ち物: { data.memo }</div>
+								<div className="close-text"><i className="material-icons left">nature_people</i>集合場所: { data.meeting }</div>
 								<div className="close-text"><i className="material-icons left">language</i>ガイドの言語：{ data.languages }</div>
 							</div>
 						</div>
 						<hr/>
 						<div className="row">
-							<div className="col s2">
+							<div className="col s3">
 								<div className="close-text">
 									ホストの<span>{ data.host }</span>さんについて
 								</div>
 							</div>
-							<div className="col s10">
+							<div className="col s9">
 								<div className="close-text">
 									{ data.host_info }
 								</div>
 							</div>
 						</div>
 						<div className="row">
-							<div className="col s2">
+							<div className="col s3">
 								<div className="close-text">
 									<span>経験</span>のできること
 								</div>
 							</div>
-							<div className="col s10">
+							<div className="col s9">
 								<div className="close-text">
 									{ data.event_info }
+								</div>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col s3">
+								<div className="close-text">
+									<span>event </span>location
+								</div>
+							</div>
+							<div className="col s9">
+								<div className="close-text">
+									<iframe className="area-map" title="map" src={ data.map_url } allowFullScreen></iframe>
 								</div>
 							</div>
 						</div>
@@ -58,7 +71,7 @@ class Life extends Component{
 						<img className="responsive-img" src={ data.main_img } alt="event promo" />
 						<div className="row no-margin-bot">
 							<div className="col s6">
-								<div className="close-text">{ data.price }</div>
+								<div className="center close-text">{ data.price }</div>
 								<div className="close-text">
 									<div className="star-ratings-css left">
 									  <div className="star-ratings-css-top" style={{width: "90%"}}><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
@@ -67,8 +80,8 @@ class Life extends Component{
 									<p className="reviews-text">12件のレビュー</p>
 								</div>
 							</div>
-							<div className="col s6">
-								<a className="btn appointment-btn orange darken-3">Book!</a>
+							<div className="col s6 center">
+								<a className="btn orange darken-3">Book!</a>
 							</div>
 						</div>
 						<div className="row no-margin-bot">
