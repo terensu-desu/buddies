10dcf4f93c3238d197df74bd606a0cf45a910ea6
Terence <tmangram@gmail.com>
2017-10-29 19:03:10 +0900

    Change to data for displaying

--- src/store.js
+++ src/store.js
@@ -6,9 +6,9 @@ class store extends EventEmitter {
 		this.data = {
 			life_data: [
 				{
-					title: "名古屋大学で就職フェア",
-					category: "Social",
-					host: "元山涼介",
+					title: "サービス購入の流れを体験",
+					category: "Services",
+					host: "Vitamin Staff",
 					date: "10/31/2017",
 					price: "¥1500 / 人",
 					img: "https://placeimg.com/300/250/people?t=1509023119587",
@@ -17,29 +17,33 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102598189",
 					time: "4時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
-					title: "地域の知識を持ってく上げる！",
-					category: "Nature/Area",
+					title: "携帯の契約に付き添います",
+					category: "Services",
 					host: "山田嘉恒",
 					date: "11/01/2017",
 					price: "¥2000 / 人",
 					img: "https://placeimg.com/300/250/any",
 					url: "/life/2",
-					subtitle: "川へフで初険しごち録性ヲルオ教代初違ヲサソヘ属額サエナウ髪器ず！",
+					subtitle: "名古屋市内の大手3社携帯電話会社への契約のお手伝いをします。",
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102675212",
-					time: "3時間",
-					memo: "持ち物、食べ物、飲み物",
+					time: "2時間",
+					memo: "身分証明書、あればご自分の銀行のキャッシュカードもお持ちください。",
+					meeting: "名古屋駅金時計前",
 					languages: "日本語、中国語、英語",
-					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					host_info: "名古屋市在住の二児の父親です。昭和区で旅館業許可書（簡易宿所）を取得してAirbnbを運営しています。普通自動車免許所持しています。簡単な英語、中国語でのやりとりは可能なので仕事の依頼もどうぞ。",
+					event_info: "日本に来たばかりの方々のために、携帯電話契約のお手伝いをします。中国語、英語での通訳ができるので日本語が出来ない方にはおすすめです！価格はお一人ずつに頂く料金となっております。通常2,3名の方をグループでお連れしています。他にも付き添いのリクエストがございましたら、お気軽にご連絡ください！",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
-					title: "銀行の口座をあけます",
-					category: "Services",
+					title: "名古屋大学周辺を案内します！",
+					category: "Nature/Area",
 					host: "張本充太郎",
 					date: "11/02/2017",
 					price: "¥3500 / 人",
@@ -49,9 +53,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102695866",
 					time: "1時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "自転車の保険を申し込みます",
@@ -65,9 +71,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/people?t=1509103025066",
 					time: "1時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "一緒に日本のレシピーを慣れます",
@@ -81,9 +89,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102846565",
 					time: "5時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "スマートフォンを申し込みましょう！",
@@ -97,9 +107,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/tech?t=1509102967412",
 					time: "2時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "紅茶を作りましょう！",
@@ -113,9 +125,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102568870",
 					time: "1時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "サバイバルの日本語を習いましょう！",
@@ -129,9 +143,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102618464",
 					time: "2時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "日本で家電を習いましょう！",
@@ -145,30 +161,34 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102559223",
 					time: "5時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 			],
 			supporter_data: [
 				{
 					supporter: "Maeda Yuna",
-					experience: "Psychologist",
-					languages: "日本語、中国語、英語",
+					experience: "メンタルサポート",
+					languages: "日本語、英語",
 					time: "10:00 - 17:30",
 					price: "¥3000 / 時間",
 					img: "https://placeimg.com/300/250/any/sepia",
 					url: "/supporter/1",
-					supporter_intro: "ゃらね以野み知擢、いあせこ素瀬根夜手阿譜さゆアチユモメラョもそ遊巣、なむまあうけもも列うさるのアクナんみりりはおももょり日知知津ろもら、等手毛派ふ「け以区」課すょ露せみる。",
+					supporter_intro: "愚痴や相談承ります！\n学業・職場でのストレスを軽減して異文化環境下でも自分らしく生活しませんか？\nまずはご相談ください！",
 					status: true,
-					skills: "Singing, dancing, acting",
+					area: "オフラインのみ。",
+					skills: "臨床心理士",
+					pic_profile: "臨床心理士の資格を持つ28歳女性です。\n大学の頃、フィリピンに留学したので英語も少し出来ます。\nどうぞよろしくお願いいたします。",
 					main_img: "https://cdn.cloudpix.co/images/atsuko-maeda/maeda-atsuko-music-4bdda7c2468ef28c2ab1d45f964396dc-large-842017.jpg",
 					thumb1: "https://i.pinimg.com/736x/41/20/7e/41207edba6e9ae4e02bde3cca75c851d--rising-sun-guitar.jpg",
 					thumb2: "https://www.generasia.com/w/images/thumb/f/ff/Atsuko_maeda_early_2007.jpg/200px-Atsuko_maeda_early_2007.jpg"
 				},
 				{
 					supporter: "Matsumoto Jon",
-					experience: "Financial Specialist",
+					experience: "財政的サポート",
 					languages: "日本語、中国語、英語",
 					time: "12:00 - 18:30",
 					price: "¥3000 / 時間",
@@ -176,7 +196,8 @@ class store extends EventEmitter {
 					url: "/supporter/2",
 					supporter_intro: "ゃらね以野み知擢、いあせこ素瀬根夜手阿譜さゆアチユモメラョもそ遊巣、なむまあうけもも列うさるのアクナんみりりはおももょり日知知津ろもら、等手毛派ふ「け以区」課すょ露せみる。",
 					status: true,
-					skills: "Singing, dancing, acting",
+					area: "オフラインのみ。",
+					skills: "公認会計士",
 					main_img: "https://i.pinimg.com/originals/0a/d2/f3/0ad2f306599b391863d06f829e55c34b.jpg",
 					thumb1: "https://i.mydramalist.com/rNOYNc.jpg",
 					thumb2: "https://i.pinimg.com/originals/9c/f6/34/9cf63400c8ad292ffb705c5f8e093cac.jpg"
@@ -191,6 +212,7 @@ class store extends EventEmitter {
 					url: "/supporter/3",
 					supporter_intro: "ゃらね以野み知擢、いあせこ素瀬根夜手阿譜さゆアチユモメラョもそ遊巣、なむまあうけもも列うさるのアクナんみりりはおももょり日知知津ろもら、等手毛派ふ「け以区」課すょ露せみる。",
 					status: true,
+					area: "オフラインのみ。",
 					skills: "Singing, dancing",
 					main_img: "https://i.pinimg.com/736x/38/95/fc/3895fc2cae55432a1a0a9e184362102b--hirai-momo-kpop.jpg",
 					thumb1: "https://i.pinimg.com/736x/c1/f3/a8/c1f3a8e80953b9bbc480d752eed616b4--hirai-momo-fangirl.jpg",
@@ -206,6 +228,7 @@ class store extends EventEmitter {
 					url: "/supporter/4",
 					supporter_intro: "ゃらね以野み知擢、いあせこ素瀬根夜手阿譜さゆアチユモメラョもそ遊巣、なむまあうけもも列うさるのアクナんみりりはおももょり日知知津ろもら、等手毛派ふ「け以区」課すょ露せみる。",
 					status: true,
+					area: "オフラインのみ。",
 					skills: "Comedy",
 					main_img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Sugichan_%22Children_of_Iron%22_at_Opening_Ceremony_of_the_28th_Tokyo_International_Film_Festival_%2821805360794%29.jpg/1200px-Sugichan_%22Children_of_Iron%22_at_Opening_Ceremony_of_the_28th_Tokyo_International_Film_Festival_%2821805360794%29.jpg",
 					thumb1: "https://www.japantimes.co.jp/wp-content/uploads/2013/05/nn20121204a8a.jpg",
@@ -221,6 +244,7 @@ class store extends EventEmitter {
 					url: "/supporter/5",
 					supporter_intro: "ゃらね以野み知擢、いあせこ素瀬根夜手阿譜さゆアチユモメラョもそ遊巣、なむまあうけもも列うさるのアクナんみりりはおももょり日知知津ろもら、等手毛派ふ「け以区」課すょ露せみる。",
 					status: true,
+					area: "オフラインのみ。",
 					skills: "Comedy, travel",
 					main_img: "https://image.tmdb.org/t/p/h632/vMe7dwdDpsT9qo55fuuoF7JkZVV.jpg",
 					thumb1: "https://hoofin.files.wordpress.com/2010/10/imoto-ayako.jpg",
@@ -236,6 +260,7 @@ class store extends EventEmitter {
 					url: "/supporter/6",
 					supporter_intro: "ゃらね以野み知擢、いあせこ素瀬根夜手阿譜さゆアチユモメラョもそ遊巣、なむまあうけもも列うさるのアクナんみりりはおももょり日知知津ろもら、等手毛派ふ「け以区」課すょ露せみる。",
 					status: true,
+					area: "オフラインのみ。",
 					skills: "Investigation, problem solving",
 					main_img: "https://i.pinimg.com/originals/50/25/65/50256523199849dd1ab2e5fb1f9d9bbb.jpg",
 					thumb1: "https://i.pinimg.com/736x/75/0a/73/750a73d5b68e14cafa20ee10263d93c7--manga-anime-anime-girls.jpg",
@@ -255,9 +280,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102511822",
 					time: "2時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "かわいい猫と写真を取りましょう！",
@@ -271,9 +298,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102723222",
 					time: "3時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "Coffee & Code: Let's プログラム！",
@@ -287,9 +316,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/tech?t=1509102941505",
 					time: "7時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "ハイクで美しい自然を感動させます。",
@@ -303,9 +334,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102744365",
 					time: "12時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "ニューヨークを探るざ！",
@@ -319,9 +352,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102585647",
 					time: "7時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "庄内緑地公園バーベキュー",
@@ -335,9 +370,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102823793",
 					time: "5時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 				{
 					title: "ビギナー写真家のレッスン",
@@ -351,9 +388,11 @@ class store extends EventEmitter {
 					main_img: "https://placeimg.com/1000/1000/any?t=1509102765608",
 					time: "4時間",
 					memo: "持ち物、食べ物、飲み物",
+					meeting: "搭監48購伐唄づめ。",
 					languages: "日本語、中国語、英語",
 					host_info: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。"
+					event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 			]
 		}
