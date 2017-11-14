ad3b785c3f9a9e760c26db00d2dc8a3a64676fef
Terence <tmangram@gmail.com>
2017-11-15 00:40:54 +0900

    Initial change to search bar component instead of banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,11 +3,21 @@ import React, { Component } from 'react'
 class Banner extends Component{
 	render() {
 		return (
-			<div className="container-fluid banner">
+			<div className="container">
 				<div className="section">
-					<div className="container center white-text">
-						<h1 className="banner-title">Vitamin</h1>
-						<h3 className="banner-subtitle">People <i className="material-icons">share</i> Time <i className="material-icons">share</i> Experiences</h3>
+					<div className="row no-margin-bot">
+						<div className="col s12">
+							<div className="card-panel">
+								<div className="row no-margin-bot">
+									<div className="input-field col s12">
+										<i className="material-icons prefix"></i>
+										<input type="search" id="searchBar" className="autocomplete" />
+										<label htmlFor="searchBar">Search Buddies</label>
+										<a className="btn-large featured-btn right">Search!</a>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
