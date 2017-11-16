fe99d88c2840e2d43dd8015cea14e807cf7a0590
Terence <tmangram@gmail.com>
2017-11-17 02:51:42 +0900

    Add class for rounded card border radius

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,13 +7,13 @@ class Banner extends Component{
 				<div className="section">
 					<div className="row no-margin-bot">
 						<div className="col s12">
-							<div className="card-panel">
+							<div className="card-panel card-round">
 								<div className="row no-margin-bot">
 									<div className="input-field col s12">
 										<i className="material-icons prefix"></i>
 										<input type="search" id="searchBar" className="autocomplete" />
 										<label htmlFor="searchBar">Search Buddies</label>
-										<a className="btn-large featured-btn right">Search!</a>
+										<a className="btn-large orange darken-3 right">Search!</a>
 									</div>
 								</div>
 							</div>
