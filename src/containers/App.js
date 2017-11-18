d9c0afcab81288573f92cd904b8ccf54de119e90
Terence Mangram <tmangram@gmail.com>
2017-11-18 20:37:54 +0900

    Add okta initialization

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import { HashRouter as Router } from 'react-router-dom' //can use HashRouter if running into trouble on builds
 import Navbar from '../components/Navbar'
 import Main from '../components/Main'
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+
+const config = {
+  issuer: 'https://dev-345698.oktapreview.com/oauth2/default',
+  redirectUri: window.location.origin + '/implicit/callback',
+  clientId: '0oacxjbp9y3dH50cI0h7'
+}
 
 class App extends Component {
   constructor() {
