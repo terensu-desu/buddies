5ac7faacfde63ce3e21884ea22fb43a490a621f4
Terence <tmangram@gmail.com>
2017-11-19 23:48:26 +0900

    Rename Banner to Search finally

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class Banner extends Component{
+class Search extends Component{
 	render() {
 		return (
 			<div className="container">
@@ -9,11 +9,13 @@ class Banner extends Component{
 						<div className="col s12">
 							<div className="card-panel card-round">
 								<div className="row no-margin-bot">
-									<div className="input-field col s12">
-										<i className="material-icons prefix"></i>
+									<div className="input-field col s12 m10 no-margin-top">
+										<i className="material-icons prefix search-margin-push">search</i>
 										<input type="search" id="searchBar" className="autocomplete" />
 										<label htmlFor="searchBar">Search Buddies</label>
-										<a className="btn orange darken-3 right">Search!</a>
+									</div>
+									<div className="col s12 m2 center">
+										<a className="btn orange darken-3 search-margin-push">Search!</a>
 									</div>
 								</div>
 							</div>
@@ -25,4 +27,4 @@ class Banner extends Component{
 	}
 }
 
-export default Banner
+export default Search
