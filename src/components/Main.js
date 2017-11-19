321a919c96efc44bfadbcf5ccac3d16058ab5cdb
Terence <tmangram@gmail.com>
2017-11-19 23:51:31 +0900

    Merged with App.js for the moment for testing.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 import FeaturedPage from '../containers/FeaturedPage'
 import LifePage from '../containers/LifePage'
 import SupporterPage from '../containers/SupporterPage'
 import EventsPage from '../containers/EventsPage'
 import Banner from '../components/Banner'
+import LoginPage from './Auth/LoginPage'
 
 class Main extends Component{
 	render() {
 		return (
-			<div>
+			<Security issuer={config.issuer}
+								client_id={config.clientId}
+								redirect_uri={config.redirect_uri}>
+        <Navbar/>
 				<Banner/>
-				<Route exact path="/" component={ FeaturedPage }/>
-				<Route path="/life/:id" component={ LifePage } />
-				<Route path="/supporter/:id" component={ SupporterPage }/>
-				<Route path="/events/:id" component={ EventsPage }/>
-			</div>
+				<Route exact path="/" component={FeaturedPage}/>
+				<Route path="/login" component={LoginPage}/>
+				<Route path="/life/:id" component={LifePage} />
+				<Route path="/supporter/:id" component={SupporterPage}/>
+				<Route path="/events/:id" component={EventsPage}/>
+			</Security>
 		)
 	}
 }
