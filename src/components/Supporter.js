fe902ae479a8c85edbe59ddd166cb2c8b6625113
Terence <tmangram@gmail.com>
2017-11-20 22:06:54 +0900

    Change to reflect store changes, cleaned up structure for unified style page

--- src/components/Supporter.js
+++ src/components/Supporter.js
@@ -6,23 +6,23 @@ class Supporter extends Component{
 		return (
 			<div className="row">
 				<div className="col s12">
-					<div className="card horizontal">
+					<div className="card horizontal card-round">
 						<div className="card-stacked">
 							<div className="card-content">
-								<h4 className="no-margin-top"><span>{ data.supporter }</span>さん</h4>
+								<h4 className="no-margin-top"><span>{data.host}</span>さん</h4>
 								<div className="row no-margin-bot">
 									<div className="col s12">
-										<div className="close-text display-linebreak">{ data.supporter_intro }</div>
+										<div className="close-text display-linebreak">{data.subtitle}</div>
 									</div>
 								</div>
 								<hr/>
 								<div className="row no-margin-bot">
 									<div className="col s12">
-										<div className="close-text"><i className="material-icons left">person</i><span>ステータス:</span> { data.status ? "オンライン!" : "オフライン" }</div>
-										<div className="close-text"><i className="material-icons left">language</i><span>ガイドの言語: </span>{ data.languages }</div>
-										<div className="close-text"><i className="material-icons left">build</i><span>経験:</span> { data.experience }</div>
-										<div className="close-text"><i className="material-icons left">school</i><span>技能:</span> { data.skills }</div>
-										<div className="close-text"><i className="material-icons left">my_location</i><span>場所:</span> { data.area }</div>
+										<div className="close-text"><i className="material-icons left">visibility</i><span>ステータス:</span> {data.status ? "オンライン!" : "オフライン"}</div>
+										<div className="close-text"><i className="material-icons left">language</i><span>ガイドの言語: </span>{data.languages}</div>
+										<div className="close-text"><i className="material-icons left">date_range</i><span>次の日:</span> {data.date}</div>
+										<div className="close-text"><i className="material-icons left">access_time</i><span>予定時刻:</span> {data.time}</div>
+										<div className="close-text"><i className="material-icons left">nature_people</i><span>集合場所:</span> {data.meeting}</div>
 										<div className="row">
 											<div className="col s12">
 												<div className="star-ratings-css left">
@@ -43,10 +43,10 @@ class Supporter extends Component{
 								<div className="row no-margin-bot">
 									<div className="col s12">
 										<div className="close-text">
-											<span>サポーターのオープンタイム</span>
+											<i className="material-icons left">note</i><span>持ち物</span>
 										</div>
 										<div className="close-text">
-											{ data.time }
+											{data.memo}
 										</div>
 										<hr/>
 									</div>
@@ -54,13 +54,15 @@ class Supporter extends Component{
 								<div className="row">
 									<div className="col s12">
 										<div className="close-text">
-											<span>サポーターのプロフィール</span>
+											<i className="material-icons left">face</i><span>サポーターのプロフィール</span>
 										</div>
 										<div className="display-linebreak close-text">
-											{ data.pic_profile }
+											{data.profile}
 										</div>
 									</div>
 								</div>
+								<br/>
+								<br/>
 								<div className="row no-margin-bot">
 									<div className="col s12 center">
 										<a href="#!"><i className="vit-accent social-icons fa fa-facebook-square fa-2x" aria-hidden="true"></i><p className="hidden">Facebook</p></a>
@@ -73,14 +75,17 @@ class Supporter extends Component{
 							</div>
 						</div>
 						<div className="card-image">
-							<img className="responsive-img page-main-img" src={ data.main_img } alt="supporter promo"/>
+							<img className="responsive-img page-main-img card-image-semi-round" src={this.state.mainImage} alt="supporter promo"/>
 							<div className="container thumbnail-container close-text">
 								<div className="row">
-									<div className="col s6">
-										<img className="thumbnail right" src={ data.thumb1 } alt="supporter promo"/>
+									<div className="col s4">
+										<img className="thumbnail hoverable right" src={data.main_img} alt="supporter promo"/>
 									</div>
-									<div className="col s6">
-										<img className="thumbnail" src={ data.thumb2 } alt="supporter promo"/>
+									<div className="col s4">
+										<img className="thumbnail hoverable right" src={data.sec_img} alt="supporter promo"/>
+									</div>
+									<div className="col s4">
+										<img className="thumbnail hoverable right" src={data.thr_img} alt="supporter promo"/>
 									</div>
 								</div>
 							</div>
