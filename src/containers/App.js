06fd064adbecb7f2ded35b47c23d2bfec5edf81a
Terence <tmangram@gmail.com>
2017-11-23 23:36:19 +0900

    Okta auth wraps App to let App handle state, DetailPageFilter instead of three different components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom' //can use HashRouter if running into trouble on builds
 import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react'
+import { withAuth } from '@okta/okta-react'
 import Navbar from '../components/Navbar'
 import FeaturedPage from '../containers/FeaturedPage'
-import LifePage from '../containers/LifePage'
-import SupporterPage from '../containers/SupporterPage'
-import EventsPage from '../containers/EventsPage'
+import DetailPageFilter from '../containers/DetailPageFilter'
 import Search from '../components/Search'
 import Protected from '../components/Protected'
 import LoginPage from '../components/LoginPage'
@@ -14,7 +13,25 @@ function onAuthRequired({history}) {
   history.push('/login')
 }
 
-class App extends Component {
+export default withAuth(class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { authenticated: null }
+    this.checkAuthentication = this.checkAuthentication.bind(this)
+    //this.checkAuthentication()
+  }
+
+  async checkAuthentication() {
+    const authenticated = await this.props.auth.isAuthenticated()
+    if(authenticated !== this.state.authenticated) {
+      this.setState({ authenticated })
+    }
+  }
+
+  componentDidUpdate() {
+    this.checkAuthentication()
+  }
+
   render() {
     return (
       <Router>
@@ -22,19 +39,15 @@ class App extends Component {
                     client_id="0oacxjbp9y3dH50cI0h7"
                     redirect_uri={window.location.origin + '/implicit/callback'}
                     onAuthRequired={onAuthRequired} >
-            <Navbar />
+            <Navbar authentication={this.state.authenticated} />
             <Search />
             <Route exact path="/" component={FeaturedPage} />
+            <Route exact path="/:page/:id" component={DetailPageFilter} />
             <SecureRoute path="/protected" component={Protected} />
             <Route path="/login" render={() => <LoginPage baseUrl="https://dev-345698.oktapreview.com" />} />
             <Route path="/implicit/callback" component={ImplicitCallback} />
-            <Route path="/life/:id" component={LifePage} />
-            <Route path="/buddies/:id" component={SupporterPage} />
-            <Route path="/events/:id" component={EventsPage} />
           </Security>
       </Router>
     )
   }
-}
-
-export default App
+})
