721913122b87cb4c3e8efb479920fe33536b5419
Terence <tmangram@gmail.com>
2017-11-26 19:57:38 +0900

    Remove section class div

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,22 +4,20 @@ class Search extends Component{
 	render() {
 		return (
 			<div className="container">
-				<div className="section">
-					<div className="row no-margin-bot">
-						<div className="col s12">
-							<div className="card-panel card-round">
-								<div className="row no-margin-bot">
-									<div className="input-field col s12 m10 no-margin-top">
-										<i className="material-icons prefix search-margin-push">search</i>
-										<input type="search" id="searchBar" className="autocomplete" />
-										<label htmlFor="searchBar">Search Buddies</label>
-										<div className="center">
-											<a className="btn orange darken-3 search-margin-push hide-on-large-only show-on-medium-only">Search!</a>
-										</div>
+				<div className="row no-margin-bot">
+					<div className="col s12">
+						<div className="card-panel card-round">
+							<div className="row no-margin-bot">
+								<div className="input-field col s12 m10 no-margin-top">
+									<i className="material-icons prefix search-margin-push">search</i>
+									<input type="search" id="searchBar" className="autocomplete" />
+									<label htmlFor="searchBar">Search Buddies</label>
+									<div className="center">
+										<a className="btn orange darken-3 search-margin-push hide-on-large-only show-on-medium-only">Search!</a>
 									</div>
-									<div className="col s12 m2 center">
-										<a className="btn orange darken-3 search-margin-push hide-on-med-and-down">Search!</a>
-									</div>
+								</div>
+								<div className="col s12 m2 center">
+									<a className="btn orange darken-3 search-margin-push hide-on-med-and-down">Search!</a>
 								</div>
 							</div>
 						</div>
