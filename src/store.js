3c73ea1a2cee3ca1041a4cc63e238024fbb77de8
Terence <tmangram@gmail.com>
2017-12-03 22:03:47 +0900

    Fixes to prop names

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { EventEmitter } from 'events'
 class store extends EventEmitter {
 	constructor() {
 		super()
-		this.daata = {
+		this.data = {
 			/*chinese: {
 				requests: {
 					buddies: {
@@ -542,7 +542,8 @@ class store extends EventEmitter {
 					navbar: {
 						sublogo: "Meet partners, connect, support.",
 						about: "About",
-						login: "Login",
+						logIn: "Log In",
+						logOut: "Log Out",
 						signUp: "Sign Up",
 					},
 					home:  {
@@ -557,6 +558,7 @@ class store extends EventEmitter {
 						secureDetail1: "Quisque nec laoreet metus. Sed commodo feugiat tellus, elementum luctus enim consequat id.",
 						secureDetail2: "Quisque nec laoreet metus. Sed commodo feugiat tellus, elementum luctus enim consequat id.",
 						secureDetail3: "Quisque nec laoreet metus. Sed commodo feugiat tellus, elementum luctus enim consequat id.",
+						more: "More",
 					},
 					categories: {
 						buddies: ["All","Transportation","City Hall","School","Hospital","Bank/Finance","Child","Hobby","Life Consultation","Cooking","Diet","Beauty/Fashion","Other"],
@@ -822,84 +824,88 @@ class store extends EventEmitter {
 				support: {
 					buddies: [
 						{
-							id: "b1i53i",
-							title: "名古屋市内の案内お願いします！",
-							category: "その他",
-							host: "伊藤杏那",
+							id: "bDz153",
+							title: "携帯の契約に付き添います",
+							category: "Services",
+							host: "山田大翔",
 							date: "11/01/2017",
-							price: "¥500/時",
-							img: "https://i.imgur.com/anNQR3T.jpg",
-							url: "/details/support/buddies/b1i53i",
-							subtitle: "川へフで初険しごち録性ヲルオ教代初違ヲサソヘ属額サエナウ髪器ず！",
-							main_img: "https://i.imgur.com/anNQR3T.jpg",
-							sec_img: "https://i.imgur.com/GIXcM8A.jpg",
-							thr_img: "https://i.imgur.com/MqHaQUP.jpg",
-							time: "5時間",
-							memo: "持ち物、食べ物、飲み物",
-							meeting: "搭監48購伐唄づめ。",
-							profile: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
-							event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
+							price: "¥2000/人",
+							img: "https://i.imgur.com/XsrlVdD.jpg",
+							url: "/details/buddies/bDz153",
+							subtitle: "名古屋市内の大手3社携帯電話会社への契約のお手伝いをします。",
+							main_img: "https://i.imgur.com/XsrlVdD.jpg",
+							sec_img: "https://i.imgur.com/khqbV6g.jpg",
+							thr_img: "https://i.imgur.com/qimhUev.jpg",
+							time: "2時間",
+							memo: "身分証明書、あればご自分の銀行のキャッシュカードもお持ちください。",
+							meeting: "名古屋駅金時計前",
 							languages: "日本語、中国語、英語",
+							profile: "名古屋市在住の二児の父親です。昭和区で旅館業許可書（簡易宿所）を取得してAirbnbを運営しています。普通自動車免許所持しています。簡単な英語、中国語でのやりとりは可能なので仕事の依頼もどうぞ。",
+							event_info: "日本に来たばかりの方々のために、携帯電話契約のお手伝いをします。中国語、英語での通訳ができるので日本語が出来ない方にはおすすめです！価格はお一人ずつに頂く料金となっております。通常2,3名の方をグループでお連れしています。他にも付き添いのリクエストがございましたら、お気軽にご連絡ください！",
+							map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I",
 						},
 						{
-							id: "L4c10y",
-							title: "保険の申し込み方",
-							category: "生活相談",
-							host: "渡辺愛莉",
+							id: "b1d5io3",
+							title: "名古屋大学周辺を案内します！",
+							category: "Nature/Area",
+							host: "中村正博",
 							date: "11/02/2017",
-							price: "¥1500/時",
-							img: "https://i.imgur.com/v1JbZ3h.jpg",
-							url: "/details/support/buddies/L4c10y",
+							price: "¥3500/人",
+							img: "https://i.imgur.com/ydqPRYH.jpg",
+							url: "/details/buddies/b1d5io3",
 							subtitle: "川へフで初険しごち録性ヲルオ教代初違ヲサソヘ属額サエナウ髪器ず！",
-							main_img: "https://i.imgur.com/v1JbZ3h.jpg",
-							sec_img: "https://i.imgur.com/FeKfXsD.jpg",
-							thr_img: "https://i.imgur.com/mZxKsxg.jpg",
+							main_img: "https://i.imgur.com/ydqPRYH.jpg",
+							sec_img: "https://i.imgur.com/hVOOHHE.jpg",
+							thr_img: "https://i.imgur.com/SZ1rBAu.jpg",
 							time: "1時間",
 							memo: "持ち物、食べ物、飲み物",
 							meeting: "搭監48購伐唄づめ。",
+							languages: "日本語、中国語、英語",
 							profile: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
 							event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
-							languages: "日本語、中国語、英語",
+							map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I",
 						},
 						{
-							id: "jW1h44",
-							title: "引っ越し手伝い募集中",
-							category: "交通",
-							host: "高橋恵介",
+							id: "Cz5e48",
+							title: "自転車の保険を申し込みます",
+							category: "Services",
+							host: "小林春菜",
 							date: "11/03/2017",
-							price: "¥2000/時",
-							img: "https://i.imgur.com/wJGbDaM.jpg",
-							url: "/details/support/buddies/jW1h44",
+							price: "¥1000/人",
+							img: "https://i.imgur.com/jznhc6d.jpg",
+							url: "/details/buddies/Cz5e48",
 							subtitle: "川へフで初険しごち録性ヲルオ教代初違ヲサソヘ属額サエナウ髪器ず！",
-							main_img: "https://i.imgur.com/wJGbDaM.jpg",
-							sec_img: "https://i.imgur.com/AP48h4L.jpg",
-							thr_img: "https://i.imgur.com/vpOMwM9.jpg",
+							main_img: "https://i.imgur.com/jznhc6d.jpg",
+							sec_img: "https://i.imgur.com/vy6cKmz.jpg",
+							thr_img: "https://i.imgur.com/FMnJMlp.jpg",
 							time: "1時間",
 							memo: "持ち物、食べ物、飲み物",
 							meeting: "搭監48購伐唄づめ。",
+							languages: "日本語、中国語、英語",
 							profile: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
 							event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
-							languages: "日本語、中国語、英語",
+							map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I",
 						},
 						{
-							id: "T8kf21",
-							title: "日本語でお話しませんか？",
-							category: "生活相談",
-							host: "鈴木美咲",
+							id: "Ff15p3",
+							title: "一緒に日本のレシピーを慣れます",
+							category: "Cooking",
+							host: "山本大輔",
 							date: "11/04/2017",
-							price: "¥2500/時",
-							img: "https://i.imgur.com/BBBfGcN.jpg",
-							url: "/details/support/buddies/T8kf21",
+							price: "¥4000/人",
+							img: "https://i.imgur.com/B49UcfC.jpg",
+							url: "/details/buddies/Ff15p3",
 							subtitle: "川へフで初険しごち録性ヲルオ教代初違ヲサソヘ属額サエナウ髪器ず！",
-							main_img: "https://i.imgur.com/BBBfGcN.jpg",
-							sec_img: "https://i.imgur.com/VLRkgd9.jpg",
-							thr_img: "https://i.imgur.com/KO2exIV.jpg",
+							main_img: "https://i.imgur.com/B49UcfC.jpg",
+							sec_img: "https://i.imgur.com/npdizQm.jpg",
+							thr_img: "https://i.imgur.com/jGz6o3d.jpg",
 							time: "5時間",
 							memo: "持ち物、食べ物、飲み物",
 							meeting: "搭監48購伐唄づめ。",
+							languages: "日本語、中国語、英語",
 							profile: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。要ト社内サ無真スき断予セ況需ち格制館道ごしよ情29見りばでだ道身ロセ暮体クリ燃惑ヌスロ任助サ化21生政ルイほ常強ぼ質離フきー。",
 							event_info: "心能モホ合選ヤ不山づ西級ハケロ類野ミユ碁際ふイ中属76平ヲツヨオ芸7恩掲くばゅ水拓丸強査断ひど。点あけ作善だにまレ欺半ミモ過収ムオ開介テア島治氏れをりイ代題らっだル芸15可げ合万テタケ著生ぞうフ可秋登ょ職律泊祥クしと。",
-							languages: "日本語、中国語、英語",
+							map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I",
 						},
 					],
 					events: [
@@ -1079,7 +1085,8 @@ class store extends EventEmitter {
 					navbar: {
 						sublogo: "仲間と出会い、つながる、支え合う",
 						about: "約",
-						login: "ログイン",
+						logIn: "ログイン",
+						logOut: "ログアウト",
 						signUp: "サインアップ",
 					},
 					home:  {
@@ -1094,6 +1101,7 @@ class store extends EventEmitter {
 						secureDetail1: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。",
 						secureDetail2: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。",
 						secureDetail3: "府ば応督ぱ生味おほその面事6針ツトロシ支話検ニシ慢生セホニ門現処75句搭監48購伐唄づめ。",
+						more: "もっと"
 					},
 					categories: {
 						buddies: ["すべて","交通","市役所","学校","病院","銀行・金融","子ども","趣味","生活相談","料理","食事","美容・ファッション","その他"],
@@ -1108,7 +1116,7 @@ class store extends EventEmitter {
 				},
 			},
 		}
-		this.data = {
+		/*this.data = {
 			buddies_data: [
 				{
 					id: "bDz153",
@@ -1449,61 +1457,19 @@ class store extends EventEmitter {
 					map_url: "https://www.google.com/maps/embed/v1/view?zoom=17&center=35.1699%2C136.9373&key=AIzaSyBASTkNgIJQFwUkpuGJC9XK_iy5cRGA4_I"
 				},
 			]
-		}
+		}*/
 	}
 	retrieveLanguageView(language) {
-		return this.daata[language].view
+		return this.data[language].view
 	}
 	retrieveData(language) {
-		return this.daata[language]
+		return this.data[language]
 	}
 	retrieveDetailData(language, section, page, id){
-		let data = this.daata[language][section][page]
+		let data = this.data[language][section][page]
 		for(var x = 0; x < data.length; x++) {
 			if(data[x].id === id) {
 				return data[x]
-			}
-		}
-	}
-	getData(){
-		return this.data
-	}
-	getPageData(page, id) {
-		switch(page) {
-			case "buddies": {
-				for(var x = 0; x < this.data.buddies_data.length; x++) {
-					if(this.data.buddies_data[x].id === id) {
-						return this.data.buddies_data[x]
-					}
-				}
-				break
-			}
-			case "requests": {
-				for(var y = 0; y < this.data.requests_data.length; y++) {
-					if(this.data.requests_data[y].id === id) {
-						return this.data.requests_data[y]
-					}
-				}
-				break
-			}
-			case "life": {
-				for(var y = 0; y < this.data.life_data.length; y++) {
-					if(this.data.life_data[y].id === id) {
-						return this.data.life_data[y]
-					}
-				}
-				break
-			}
-			case "event": {
-				for(var z = 0; z < this.data.event_data.length; z++) {
-					if(this.data.event_data[z].id === id) {
-						return this.data.event_data[z]
-					}
-				}
-				break
-			}
-			default: {
-				break
 			}
 		}
 	}
